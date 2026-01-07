a2a3a3ee70d6e8b48d786ba1720a61aedf1ae1a4
revenantonthemission <corpseonthemission@icloud.com>
2026-01-07 16:01:23 +0900

    chore: 주석 마무리

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers.user_router import router as user_router
 
 app = FastAPI()
+
+# 메인 어플리케이션에 라우터 포함
 app.include_router(user_router, tags=["users"])
 
+# 허용된 origin 목록
 origins = [
     "http://localhost",
     "http://localhost:8000",
 ]
 
+# CORSMiddleware 추가
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
