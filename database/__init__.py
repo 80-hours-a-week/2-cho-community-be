9709f5462435c1d2ea788828a326938b84fa5196
revenantonthemission <corpseonthemission@icloud.com>
2026-02-25 11:01:31 +0900

    fix: __init__.py 추가

---
+++ database/__init__.py
@@ -0,0 +1,6 @@
+"""database: 데이터베이스 연결 및 관리 패키지.
+
+Modules:
+    connection: MySQL 연결 풀 관리, 트랜잭션 컨텍스트 매니저
+    seed_data: 테스트/개발용 시드 데이터
+"""
---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+"""utils: 유틸리티 함수들을 모아놓은 패키지.
+
+Modules:
+    password: 비밀번호 해싱 및 검증
+    file_utils: 파일 업로드 처리
+    formatters: 날짜/시간 포맷팅
+    exceptions: HTTP 에러 헬퍼
+    s3_utils: AWS S3 이미지 URL 빌드
+    storage: 로컬 파일 저장소
+"""
